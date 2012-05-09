8635707b11324281de6752f96e679884413c96f8
David Sheldrick <d.j.sheldrick@gmail.com>
2012-05-09 10:42:58 +0100

    added another unit test for RNG

--- test/debug/DebugRNG-test.js
+++ test/debug/DebugRNG-test.js
@@ -1,6 +1,6 @@
 var imports = require("../../src/debug/DebugRNG.js");
 
-var rng = new imports.RandomNumberGenerator();
+
 
 var correctNumbers = [7193, 2932, 10386, 5575, 100, 15976, 430, 9740, 9449, 
                       1636, 11030, 9848, 13965, 16051, 14483, 6708, 5184, 15931,
@@ -15,8 +15,9 @@ var correctNumbers = [7193, 2932, 10386, 5575, 100, 15976, 430, 9740, 9449,
                       9053, 5881, 3867, 10512, 4312, 8529, 1576, 15803, 5498, 
                       12730, 7397];
 
-exports["Check that numbers are the same as in the project spec"] = function (test) {
+exports["Check that first 100 numbers are the same as in the project spec"] = function (test) {
 	test.expect(100);
+	var rng = new imports.RandomNumberGenerator();
 	var results = 100;
 	// timeout incase of badness
 	var timeout = setTimeout(function(){
@@ -36,5 +37,37 @@ exports["Check that numbers are the same as in the project spec"] = function (te
 			};
 		}(correctNumbers[i]));
 	}
-	
+};
+
+
+var correctNumbers2 = [ 0, 0, 0, 3, 0, 4, 3, 4, 8, 6, 8, 8, 3, 7, 8, 4, 16, 1,
+                        3, 1, 10, 4, 20, 19, 9, 14, 3, 25, 1, 6, 1, 15, 2, 5,
+                        30, 1, 1, 25, 4, 17, 4, 2, 13, 16, 30, 15, 14, 41, 32,
+                        12, 0, 38, 12, 40, 47, 54, 40, 44, 51, 51, 25, 0, 38,
+                        49, 52, 4, 63, 59, 7, 68, 40, 17, 9, 67, 10, 2, 65, 48,
+                        63, 54, 1, 12, 44, 51, 8, 80, 23, 41, 45, 53, 57, 3, 3,
+                        82, 74, 40, 89, 10, 58, 97 ];
+
+exports["Check that first 100 integers give the correct results"] = function (test) {
+	test.expect(100);
+	var rng = new imports.RandomNumberGenerator();
+	var results = 100;
+	// timeout incase of badness
+	var timeout = setTimeout(function(){
+		rng.close();
+		test.done();
+	},3000);
+
+	for (var i=0;i<100;i++) {
+		rng.next(i+1,function (expectedResult) {
+			return function (result) {
+				test.strictEqual(expectedResult,result,expectedResult+" expected, "+result+" seen");
+				if (--results === 0) {
+					rng.close();
+					clearTimeout(timeout);
+					test.done();
+				}
+			};
+		}(correctNumbers2[i]));
+	}
 };
