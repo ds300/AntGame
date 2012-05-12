99f92a46c9193714c17af88e0a8bf205870bca29
David Sheldrick <d.j.sheldrick@gmail.com>
2012-05-12 16:55:34 +0100

    started work on Ant and AntBrain classes

---
+++ src/Ant.js
@@ -0,0 +1,76 @@
+function Ant(id, color, brain, world) {
+
+	// initialise variables
+	this.color = color;
+	this.otherColor = color === "red" ? "black" : "red";
+	this.id = id;
+	this.row = 0;
+	this.col = 0;
+	this.state = 0;
+	this.dir = 0;
+	this.resting = 0;
+	this.food = 0;
+
+	this.kill = function () {
+		var cell = world.getCell(this.row,this.col);
+		cell.depositFood(3);
+		cell.removeAnt();
+		// remove from ants
+		this.step = function() {};
+	};
+
+
+	this.hasFood = function () {
+		return this.food === 1; 
+	};
+
+	this.checkForDeath = function () {
+		var count = 0;
+		for (var d = 0; d < 6; d++) {
+			if (!world.getAdjacentCell(this.row, this.col, d).containsAntOfColor(this.otherColor)){
+				count++;
+				if (count > 1) {
+					return;
+				}
+			}
+		}
+		this.kill();
+	};
+
+	this.checkForAdjacentDeaths = function () {
+		var adjCells = world.getAllAdjacentCells(this.row, this.col);
+		var enemies = new Array();
+		for (var i = 0; i < adjCells.length; i++) {
+			if (adjCells[i].containsAntOfColor(this.otherColor)) {
+				enemies.push(adjCells[i].getAnt());
+			}
+		}
+		if (enemies.length > 4) {
+			this.kill();
+		} else {
+			for (var i = 0; i < enemies.length; i++) {
+				enemies[i].checkForDeath();
+			}
+		}
+	};
+
+	this.getAdjacentCell = function (dir) {
+		return world.getAdjacentCell(this.row, this.col, dir);
+	};
+
+	this.getCurrentCell = function () {
+		return world.getCell(this.row, this.col);
+	};
+
+	this.step = function(){
+		if (this.resting > 0){
+			this.resting--;
+		} else {
+			brain[this.state](this);
+		}
+	};
+
+	this.toString = function () {
+		return this.color + " ant of id "+this.id+", dir "+this.dir+", food "+this.food+", state "+this.state+", resting "+this.resting;
+	}
+}
---
+++ src/AntBrain.js
@@ -0,0 +1,137 @@
+function AntBrain (states, color, rng) {
+	var otherColor = color === "red" ? "black" : "red";
+	var senseConditionEvaluators = {
+		"Friend": function (senseCell) {
+			return senseCell.containsAntOfColor(color); 
+		},
+		"Foe": function (senseCell) {
+			return senseCell.containsAntOfColor(otherColor); 
+		},
+		"FriendWithFood": function (senseCell) {
+			return senseCell.containsAntOfColorWithFood(color); 
+		},
+		"FoeWithFood": function (senseCell) {
+			return senseCell.containsAntOfColorWithFood(otherColor); 
+		},
+		"Food": function (senseCell) {
+			return senseCell.hasFood(); 
+		},
+		"Rock": function (senseCell) {
+			return senseCell.type==="rock"; 
+		},
+		"Marker": function (senseCell) {
+			return senseCell.hasMarker(color,marker); 
+		},
+		"FoeMarker": function (senseCell) {
+			return senseCell.hasMarker(otherColor);
+		},
+		"Home": function (senseCell) {
+			return senseCell.type === color+" hill"; 
+		},
+		"FoeHome": function (senseCell) {
+			return senseCell.type === otherColor+" hill"; 
+		}
+	};
+
+	var senseCellFinders = {
+		"Here": function (ant) { return ant.getCurrentCell(); },
+		"Ahead": function (ant) { return ant.getAdjacentCell(ant.dir); },
+		"LeftAhead": function (ant) { return ant.getAdjacentCell((ant.dir+5)%6); },
+		"RightAhead": function (ant) { return ant.getAdjacentCell((ant.dir+1)%6); }
+	};
+	var instructions = {
+		"Sense": function (state) {
+			var getSenseCell = senseCellFinders[state.dir];
+			var senseSuccess = senseConditionEvaluators[state.cond];
+			return function (ant) {
+				if (senseSuccess(getSenseCell(ant))) {
+					ant.state = state.st1;
+				} else {
+					ant.state = state.st2;
+				}
+			};
+		},
+		"Mark": function (state) {
+			return function (ant) {
+				ant.getCurrentCell().addMarker(ant.color,state.marker);
+				ant.state = state.st;
+			};
+		},
+		"Unmark": function (state) {
+			return function (ant) {
+				ant.getCurrentCell().removeMarker(ant.color,state.marker);
+				ant.state = state.st;
+			};
+		},
+		"PickUp": function (state) {
+			return function (ant) {
+				var cell = ant.getCurrentCell();
+				if (cell.hasFood() && !ant.hasFood()){
+					cell.removeFood();
+					ant.food = 1;
+					ant.state = state.st1;
+				} else {
+					ant.state = state.st2;
+				}
+			};
+		},
+		"Drop": function (state) {
+			return function (ant) {
+				if (ant.food === 1) {
+					ant.getCurrentCell().depositFood();
+					ant.food = 0;
+					ant.dropCallback();
+				}
+				ant.state = state.st;
+			};
+		},
+		"Turn": function (state) {
+			var turnAnt;
+			if (state.dir === "Left") {
+				turnAnt = function (ant) { ant.dir = (ant.dir + 5) % 6; };
+			} else {
+				turnAnt = function (ant) { ant.dir = (ant.dir + 1) % 6; };
+			}
+			return function (ant) {
+				turnAnt(ant);
+				ant.st = state.st;
+			};
+		},
+		"Move": function (state) {
+			return function (ant) {
+				var cell = ant.getAdjacentCell(ant.dir);
+				if (cell.isAvailable()){
+					cell.moveAntHere(ant);
+					ant.state = state.st1;
+					ant.resting = 14;
+					ant.checkForAdjacentDeaths();
+				} else {
+					ant.state = state.st2;
+				}
+			};
+		},
+		"Flip": function (state) {
+			return function (ant) {
+				rng.next(state.p,function(result){
+					if (result === 0){
+						ant.state = st1;
+					} else {
+						ant.state = st2;
+					}
+				});
+			};
+		}
+	};
+
+	var brain = [];
+	for (var i = 0; i < states.length; i++) {
+		brain.push(instructions[states[i].type](states[i]));
+	}
+	return brain;
+}
+
+
+
+
+
+
