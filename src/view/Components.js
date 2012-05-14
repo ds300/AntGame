4e8245134c2d616f3c77e656669bb9a19e457bbf
David Sheldrick <d.j.sheldrick@gmail.com>
2012-05-14 21:51:24 +0100

    More work on html interface

--- src/view/Components.js
+++ src/view/Components.js
@@ -1,42 +1,48 @@
-var dgid = dgid || function (id) { return document.getElementById(id); };
-var dgc = dgc || function (cn) { return document.getElementsByClassName(cn); };
+var buttonCallback = function (callback) {
+	return function (evnt) {
+		evnt && evnt.preventDefault();
+		callback && callback();
+	};
+};
 
-// gather elements
-var e = {};
-e["main_menu"] = dgc("ag-menu");
+// gather main view elements
+var elements = {};
+elements["main_menu"] = $(".ag-menu");
+elements["single_match_setup"] = $(".ag-singleMatch");
 
 
-function _hide(elems) {
-	for (var i = 0, len = elems.length; i < len; i++) {
-		var elemStyle = elems[i].style;
-		if (elemStyle.display !== "none") {
-			elemStyle.display_backup = elemStyle.display;
-			elemStyle.display = "none";
-		}
-	}
-}
 
-function _show(elems) {
-	for (var i = 0, len = elems.length; i < len; i++) {
-		var elemStyle = elems[i].style;
-		if (elemStyle.display === "none") {
-			elemStyle.display = elemStyle.display_backup || "block";
-		}
-	}
-}
+
+// gather event emitters
+var eventers = {};
+eventers["goto_main_menu"] = $(".ag-btn-menu");
+eventers["goto_single_match"] = $(".ag-btn-singleMatch");
+eventers["goto_contest"] = $(".ag-btn-contest");
 
 exports.hideAll = function () {
-	for (var elems in e) {
-		if (e.hasOwnProperty(elems)) {
-			_hide(e[elems]);
+	for (var elems in elements) {
+		if (elements.hasOwnProperty(elems)) {
+			elements[elems].hide();
 		}
 	}
-}
+	$("#ag-error").hide();
+};
 
 exports.hide = function (elemString) {
-	e[elemString] && _hide(e[elemString]);
-}
+	elements[elemString] && elements[elemString].hide();
+};
 
 exports.show = function (elemString) {
-	e[elemString] && _show(e[elemString]);
-}
+	elements[elemString] && elements[elemString].show();
+};
+
+exports.on = function (evnt, callback) {
+	eventers[evnt] && eventers[event].click(callback);
+};
+
+exports.error = function (header, body, onClose) {
+	$("#ag-error-header").html(header);
+	$("#ag-error-body").html(body);
+	$("#ag-error").on("hide", onClose);
+	$("#ag-error").modal("show");
+};
