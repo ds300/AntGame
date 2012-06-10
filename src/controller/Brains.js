5523b302fb39a478eae63016a1cf570908e311cf
David Sheldrick <d.j.sheldrick@gmail.com>
2012-06-10 14:23:20 +0100

    continued work on documentation. changed builtin brains/worlds

--- src/controller/Brains.js
+++ src/controller/Brains.js
@@ -4,7 +4,7 @@
 var BRAINS = [];
 
 BRAINS.push({
-	name: "Chealsea",
+	name: "Advanced Brain",
 	preset: true,
 	source: 
 	"Flip 10 6 1" + "\n" +
@@ -412,7 +412,7 @@ BRAINS.push({
 });
 
 BRAINS.push({
-	name: "Bolton Wanderers",
+	name: "Primitive Brain",
 	preset: true,
 	source: 
 		"Sense Ahead 1 3 Food" + "\n" +
@@ -432,26 +432,3 @@ BRAINS.push({
 		"Turn Right 8" + "\n" +
 		"Move 8 11" + "\n" 
 });
-
-BRAINS.push({
-	name: "Civic Duty",
-	preset: true,
-	source: 
-		"Sense Ahead 1 3 Food" + "\n" +
-		"Move 2 0" + "\n" +
-		"PickUp 8 0" + "\n" +
-		"Flip 3 4 5" + "\n" +
-		"Turn Left 0" + "\n" +
-		"Flip 2 6 7" + "\n" +
-		"Turn Right 0" + "\n" +
-		"Move 0 3" + "\n" +
-		"Sense Ahead 9 11 Home " + "\n" +
-		"Move 10 8" + "\n" +
-		"Drop 0" + "\n" +
-		"Flip 3 12 13" + "\n" +
-		"Turn Left 8" + "\n" +
-		"Flip 2 14 15" + "\n" +
-		"Turn Right 8" + "\n" +
-		"Move 8 11" + "\n" 
-});
-
